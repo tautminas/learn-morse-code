d7e4df37028cce6d766b9233605a39f53f99974b
Tautminas Cibulskis <tautminas@yahoo.com>
2023-09-15 00:42:17 +0300

    Add hide button functionality

--- option1_page.py
+++ option1_page.py
@@ -1,23 +1,20 @@
 import tkinter as tk
 from tkinter import ttk
-from PIL import Image, ImageFilter, ImageTk
 
 
 class Option1Page(tk.Frame):
     def __init__(self, parent, switch_callback):
         super().__init__(parent)
         self.parent = parent
+        self.text_widget_visible = None
         self.pack(fill="both", expand=True)
         self.switch_callback = switch_callback
 
         self.button = ttk.Button(self, text="Back to Main Page", command=self.switch_callback)
-        self.button.pack(anchor="w")
 
         self.hide_show_button = ttk.Button(self, text="Hide info", command=self.hide_info)
-        self.hide_show_button.pack()
 
         self.story_label = ttk.Label(self, text="Story to memorize all the alphabet in frequency order:")
-        self.story_label.pack()
 
         story_text = (
             "There is an extraterrestrial (E.T.) named IAN. He is remarkably open-minded, and "
@@ -37,33 +34,64 @@ class Option1Page(tk.Frame):
 
         self.highlight_letters_in_order()
 
-        self.text_widget.pack()
-
         self.label2 = ttk.Label(self, text="Frequency order: E T I A N M S U R W D K G O H V F L P J B X C Y Z Q")
-        self.label2.pack()
 
         self.logo_image = tk.PhotoImage(file='./assets/binary-tree.png')
         self.label = ttk.Label(self, image=self.logo_image)
-        self.label.pack()
 
-        row_frame = ttk.Frame(self)
-        row_frame.pack()
+        self.row_frame = ttk.Frame(self)
 
-        self.test_label = ttk.Label(row_frame, text="Write the letters of frequency method:")
-        self.test_label.pack(side="left")
+        self.test_label = ttk.Label(self.row_frame, text="Write the letters of frequency method:")
 
-        self.entry = tk.Entry(row_frame, width=30)
+        self.entry = tk.Entry(self.row_frame, width=30)
         self.entry.insert(0, "Some text to begin with.")
-        self.entry.pack(side="left")
 
-        self.test_button = ttk.Button(row_frame, text="Test answer", command=self.switch_callback)
-        self.test_button.pack(side="left")
+        self.test_button = ttk.Button(self.row_frame, text="Test answer", command=self.switch_callback)
 
-        self.show_answer_button = ttk.Button(row_frame, text="Show answer", command=self.switch_callback)
-        self.show_answer_button.pack(side="left")
+        self.show_answer_button = ttk.Button(self.row_frame, text="Show answer", command=self.switch_callback)
 
-        self.story_label = ttk.Label(self, text="Your answer is...")
+        self.answer_label = ttk.Label(self, text="Your answer is...")
+
+        self.pack_elements()
+
+
+    def pack_elements(self, all=True):
+        self.button.pack(anchor="w")
+        self.hide_show_button.pack()
         self.story_label.pack()
+        self.text_widget.pack()
+        self.label2.pack()
+        self.label.pack()
+        self.row_frame.pack()
+        self.test_label.pack(side="left")
+        self.entry.pack(side="left")
+        self.test_button.pack(side="left")
+        self.show_answer_button.pack(side="left")
+        self.answer_label.pack()
+
+    def forget_elements(self):
+        self.button.pack_forget()
+        self.hide_show_button.pack_forget()
+        self.story_label.pack_forget()
+        self.text_widget.pack_forget()
+        self.label2.pack_forget()
+        self.label.pack_forget()
+        self.row_frame.pack_forget()
+        self.test_label.pack_forget()
+        self.entry.pack_forget()
+        self.test_button.pack_forget()
+        self.show_answer_button.pack_forget()
+        self.answer_label.pack_forget()
+
+    def pack_non_info_elements(self):
+        self.button.pack(anchor="w")
+        self.hide_show_button.pack()
+        self.row_frame.pack()
+        self.test_label.pack(side="left")
+        self.entry.pack(side="left")
+        self.test_button.pack(side="left")
+        self.show_answer_button.pack(side="left")
+        self.answer_label.pack()
 
     def highlight_letters_in_order(self):
         letters_to_highlight = "ETIANMSURWDKGOHVFLPJBXCYZQ"
@@ -79,9 +107,11 @@ class Option1Page(tk.Frame):
 
     def hide_info(self):
         current_text = self.hide_show_button.cget("text")
-        new_text = "Hide Info" if current_text == "Show Info" else "Show Info"
+        new_text = "Hide info" if current_text == "Show info" else "Show info"
         self.hide_show_button.config(text=new_text)
 
-        # self.label.pack_forget()
-        # for child in self.parent.winfo_children():
-        #     child.pack_forget()
+        self.forget_elements()
+        if current_text == "Hide info":
+            self.pack_non_info_elements()
+        else:
+            self.pack_elements()
