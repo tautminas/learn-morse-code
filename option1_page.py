90cf62e406beed3a74f2e1c43cf02eb142650663
Tautminas Cibulskis <tautminas@yahoo.com>
2023-09-14 23:02:52 +0300

    Change layout

--- option1_page.py
+++ option1_page.py
@@ -6,19 +6,18 @@ from PIL import Image, ImageFilter, ImageTk
 class Option1Page(tk.Frame):
     def __init__(self, parent, switch_callback):
         super().__init__(parent)
+        self.parent = parent
         self.pack(fill="both", expand=True)
         self.switch_callback = switch_callback
 
         self.button = ttk.Button(self, text="Back to Main Page", command=self.switch_callback)
-        self.button.grid(row=0, column=0, sticky="w")
+        self.button.pack(anchor="w")
 
-        self.row_frame = tk.Frame(self)
-        self.row_frame.grid(row=1, column=0, sticky="nsew")
-        self.row_frame.grid_rowconfigure(0, weight=1)
-        self.row_frame.grid_columnconfigure(0, weight=1)
+        self.hide_show_button = ttk.Button(self, text="Hide info", command=self.hide_info)
+        self.hide_show_button.pack()
 
-        self.story_label = ttk.Label(self.row_frame, text="Story to memorize all the alphabet in frequency order:")
-        self.story_label.grid(row=0, column=0, sticky="w")
+        self.story_label = ttk.Label(self, text="Story to memorize all the alphabet in frequency order:")
+        self.story_label.pack()
 
         story_text = (
             "There is an extraterrestrial (E.T.) named IAN. He is remarkably open-minded, and "
@@ -31,54 +30,58 @@ class Option1Page(tk.Frame):
             "happens to be the standard size for all boxes."
         )
 
-        self.text_widget = tk.Text(self.row_frame, wrap=tk.WORD, width=50, height=13)
+        self.text_widget = tk.Text(self, wrap=tk.WORD, width=50, height=13)
         self.text_widget.insert("1.0", story_text)
         self.text_widget.config(state=tk.DISABLED)
         self.text_widget.tag_configure("highlight", background="yellow")
 
-        def highlight_letters_in_order():
-            letters_to_highlight = "ETIANMSURWDKGOHVFLPJBXCYZQ"
-            start_index = "1.0"
+        self.highlight_letters_in_order()
 
-            for letter in letters_to_highlight:
-                start_index = self.text_widget.search(letter, start_index, stopindex="end")
-                if not start_index:
-                    break
-                end_index = self.text_widget.index(f"{start_index}+1c")
-                self.text_widget.tag_add("highlight", start_index, end_index)
-                start_index = end_index
+        self.text_widget.pack()
 
-        highlight_letters_in_order()
-
-        self.text_widget.grid(row=1, column=0, sticky="w")
-
-        self.label2 = ttk.Label(self.row_frame, text="Frequency order: E T I A N M S U R W D K G O H V F L P J B X C Y Z Q")
-        self.grid(row=2, column=0, sticky="w")
+        self.label2 = ttk.Label(self, text="Frequency order: E T I A N M S U R W D K G O H V F L P J B X C Y Z Q")
+        self.label2.pack()
 
         self.logo_image = tk.PhotoImage(file='./assets/binary-tree.png')
-        self.label = ttk.Label(self.row_frame, image=self.logo_image)
-        self.label.grid(row=3, column=0, sticky="w")
+        self.label = ttk.Label(self, image=self.logo_image)
+        self.label.pack()
 
-        self.hide_show_button = ttk.Button(self, text="Hide info", command=self.hide_info)
-        self.hide_show_button.grid(row=4, column=0, sticky="w")
+        row_frame = ttk.Frame(self)
+        row_frame.pack()
 
-        self.test_label = ttk.Label(self, text="Write :")
-        self.test_label.grid(row=5, column=0, sticky="w")
+        self.test_label = ttk.Label(row_frame, text="Write the letters of frequency method:")
+        self.test_label.pack(side="left")
 
-        self.entry = tk.Entry(self, width=30)
+        self.entry = tk.Entry(row_frame, width=30)
         self.entry.insert(0, "Some text to begin with.")
-        self.entry.grid(row=6, column=0, sticky="w")
+        self.entry.pack(side="left")
 
-        self.hide_button = ttk.Button(self, text="Test answer", command=self.switch_callback)
-        self.hide_button.grid(row=7, column=0, sticky="w")
+        self.test_button = ttk.Button(row_frame, text="Test answer", command=self.switch_callback)
+        self.test_button.pack(side="left")
+
+        self.show_answer_button = ttk.Button(row_frame, text="Show answer", command=self.switch_callback)
+        self.show_answer_button.pack(side="left")
+
+        self.story_label = ttk.Label(self, text="Your answer is...")
+        self.story_label.pack()
+
+    def highlight_letters_in_order(self):
+        letters_to_highlight = "ETIANMSURWDKGOHVFLPJBXCYZQ"
+        start_index = "1.0"
+
+        for letter in letters_to_highlight:
+            start_index = self.text_widget.search(letter, start_index, stopindex="end")
+            if not start_index:
+                break
+            end_index = self.text_widget.index(f"{start_index}+1c")
+            self.text_widget.tag_add("highlight", start_index, end_index)
+            start_index = end_index
 
     def hide_info(self):
-        if self.row_frame.winfo_viewable():
-            prev_row = self.row_frame.grid_info()["row"]
-            prev_column = self.row_frame.grid_info()["column"]
-            self.row_frame.grid_forget()
+        current_text = self.hide_show_button.cget("text")
+        new_text = "Hide Info" if current_text == "Show Info" else "Show Info"
+        self.hide_show_button.config(text=new_text)
 
-            self.prev_row = prev_row
-            self.prev_column = prev_column
-        else:
-            self.row_frame.grid(row=self.prev_row, column=self.prev_column, sticky="nsew")
+        # self.label.pack_forget()
+        # for child in self.parent.winfo_children():
+        #     child.pack_forget()
